c356eae4401099d6ed0225396851b463283df075
yasserprogamer <ouahmedyasser@gmail.com>
2024-06-14 03:56:18 +0100

    Abra Cadabra!

---
+++ Makefile
@@ -0,0 +1,4 @@
+c=g++
+
+run:
+	$(c) -g main.cpp -o meme.exe
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Hi lars! Send me a message" << endl;
+    return 0;
+
+}
